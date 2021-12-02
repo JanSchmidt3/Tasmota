31936fa758debad5bddfeab13d8ebbf9d8ba5300
gemu2015 <gmutz2010@googlemail.com>
2021-12-02 19:50:36 +0100

    Update xdrv_13_display.ino

--- tasmota/xdrv_13_display.ino
+++ tasmota/xdrv_13_display.ino
@@ -96,6 +96,11 @@ void Get_display(uint8_t index) {
 }
 #endif // USE_MULTI_DISPLAY
 
+#ifndef TXT_MAX_SFAC
+#define TXT_MAX_SFAC 4
+#endif // TXT_MAX_SFAC
+
+
 const uint8_t DISPLAY_MAX_DRIVERS = 32;        // Max number of display drivers/models supported by xdsp_interface.ino
 const uint8_t DISPLAY_MAX_COLS = 64;           // Max number of columns allowed with command DisplayCols
 const uint8_t DISPLAY_MAX_ROWS = 64;           // Max number of lines allowed with command DisplayRows
@@ -876,6 +881,7 @@ void DisplayText(void)
           case 's':
             // size sx
             var = atoiv(cp, &temp);
+            if (temp > TXT_MAX_SFAC) temp = TXT_MAX_SFAC;
             if (renderer) renderer->setTextSize(temp);
             //else DisplaySetSize(*cp&3);
             cp+=var;
@@ -2012,7 +2018,7 @@ void CmndDisplayDimmer(void) {
 }
 
 void CmndDisplaySize(void) {
-  if ((XdrvMailbox.payload > 0) && (XdrvMailbox.payload <= 4)) {
+  if ((XdrvMailbox.payload > 0) && (XdrvMailbox.payload <= TXT_MAX_SFAC)) {
     Settings->display_size = XdrvMailbox.payload;
     if (renderer) renderer->setTextSize(Settings->display_size);
     //else DisplaySetSize(Settings->display_size);
