938a47f454ac01dceb5b7e589ea5f21b24eeed4a
gemu2015 <gmutz2010@googlemail.com>
2022-03-28 08:34:12 +0200

    google charts "cnth"

--- tasmota/xdrv_10_scripter.ino
+++ tasmota/xdrv_10_scripter.ino
@@ -8923,12 +8923,19 @@ exgc:
 
         int16_t divflg = 1;
         int16_t todflg = -1;
+        uint8_t hmflg = 0;
         if (!strncmp(label, "cnt", 3)) {
           char *cp = &label[3];
+          if (*cp == 'h') {
+            hmflg = 1;
+            cp++;
+          }
           //todflg=atoi(&label[3]);
           todflg = strtol(cp, &cp, 10);
-          if (todflg >= entries) todflg = entries - 1;
-          if (todflg < 0) todflg = 0;
+          if (!hmflg) {
+            if (todflg >= entries) todflg = entries - 1;
+            if (todflg < 0) todflg = 0;
+          }
           if (*cp=='/') {
             cp++;
             divflg = strtol(cp, &cp, 10);
@@ -8957,7 +8964,7 @@ exgc:
           if (todflg >= 0) {
             sprintf(lbl, "%d:%02d", todflg / divflg, (todflg % divflg) * (60 / divflg) );
             todflg++;
-            if (todflg >= entries) {
+            if ((hmflg == 0) && (todflg >= entries)) {
               todflg = 0;
             }
           } else {
