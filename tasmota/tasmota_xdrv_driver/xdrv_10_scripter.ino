b8c049c7d663cbb6f2c155bf84118326285f194c
gemu2015 <gmutz2010@googlemail.com>
2022-08-06 12:51:50 +0200

    Update xdrv_10_scripter.ino

--- tasmota/tasmota_xdrv_driver/xdrv_10_scripter.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_10_scripter.ino
@@ -3834,7 +3834,7 @@ chknext:
 #endif
 */
 
-#ifdef ESP32
+//#ifdef ESP32
         if (!strncmp(lp, "rr(", 3)) {
           lp+=4;
           len = 0;
@@ -3848,7 +3848,7 @@ chknext:
           }
           goto strexit;
         }
-#endif
+//#endif
         break;
 
       case 's':
